b84268705b9b6c320f6ebaa43d61fac5e426d449
Vien Van <vienvonvan@gmail.com>
2016-03-03 12:23:55 -0800

    Created artist model

---
+++ models/Artist.js
@@ -0,0 +1,15 @@
+//require mongoose
+var mongoose = require('mongoose');
+
+
+var ArtistSchema = new mongoose.Schema({
+    name: String,
+    hitSong: String,
+    album: String,
+    bio: String,
+    photo_url: String,
+    createdAt: {type: Date, default: new Date()}
+})
+
+var Artist = mongoose.model("Artist", ArtistSchema);
+module.exports = Artist;
