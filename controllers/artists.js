a72c6821ee482dfbad8e6f6494f0b4934b79d9cc
Vien Van <vienvonvan@gmail.com>
2016-03-03 14:58:52 -0800

    Added show router

--- controllers/artists.js
+++ controllers/artists.js
@@ -2,16 +2,19 @@ var Artist = require('../models/Artist')
 
 var artistsController = {
   index: function(req, res) {
-      res.render('artists/index', {artists:artists});
+      res.render('artists', {artists:artists});
   },
   create: function(req, res) {
-
+      Artist.create()
   },
   new: function(req, res) {
-
+      res.render('artists/new');
   },
   show: function(req, res) {
-
+      var id = req.params.id;
+      Artist.find({_id: id}, function(err, artist) {
+      err ? res.redirect('/artists') : res.render('artists/show', {artist: artist})
+      })
   },
   update: function(req, res) {
 
