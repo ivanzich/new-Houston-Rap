ad77ca0197485700def52ee2c86d3bb7deda2473
thaddak0 <annabellethaddeus@gmail.com>
2016-03-04 15:22:34 -0800

    add album model

--- models/Artist.js
+++ models/Artist.js
@@ -1,14 +1,25 @@
 //require mongoose
 var mongoose = require('mongoose');
 
+var Schema = mongoose.Schema;
+var Album = require('../models/Album')
 
-var ArtistSchema = new mongoose.Schema({
+// embedding data for album in artist
+var AlbumSchema = new Schema({
     name: String,
-    album: String,
+    yearReleased: String,
+    songCount: Number
+});
+
+var ArtistSchema = new Schema({
+    name: String,
+    Album: [AlbumSchema],
     bio: String,
     photo_url: String,
     createdAt: {type: Date, default: new Date()}
-})
+});
 
 var Artist = mongoose.model("Artist", ArtistSchema);
+
+// export
 module.exports = Artist;
