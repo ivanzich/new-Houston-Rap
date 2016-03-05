6adb25092ba4d4d53c44f910369b432d0ccc4a00
Vien Van <vienvonvan@gmail.com>
2016-03-04 16:09:10 -0800

    resolved more conflicts

--- models/Artist.js
+++ models/Artist.js
@@ -1,18 +1,11 @@
 //require mongoose
 var mongoose = require('mongoose');
 
-<<<<<<< HEAD
-var ArtistSchema = new mongoose.Schema({
-||||||| merged common ancestors
-
-var ArtistSchema = new mongoose.Schema({
-=======
 var Schema = mongoose.Schema;
 var Album = require('../models/Album')
 
 // embedding data for album in artist
 var AlbumSchema = new Schema({
->>>>>>> master
     name: String,
     yearReleased: String,
     songCount: Number
